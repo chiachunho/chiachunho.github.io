dbeca1429fb10119d2cbf92e6e7d55b6ffd06df8
Jeffery Ho <chiachun2491@gmail.com>
2023-12-12 17:03:17 +0800

    feat: Quick Links

--- pages/index.js
+++ pages/index.js
@@ -28,61 +28,77 @@ export default function Home() {
         <meta property="og:image:width" content="2300" />
         <meta property="og:image:height" content="2300" />
       </Head>
-      <div className="container mx-auto min-h-screen flex flex-col lg:flex-row items-center justify-center gap-3 md:gap-6 p-2 md:p-6">
-        <div className="w-1/3 md:w-1/3  max-w-[200px]">
-          <div className="rounded-full aspect-square leading-[0] shadow-lg border-8 border-white">
-            <Image
-              src="/images/jeffery.jpeg"
-              className="rounded-full"
-              alt="jeffery"
-              width={0}
-              height={0}
-              sizes="100vw"
-              loader={customImageLoader}
-              unoptimized
-            ></Image>
+      <div className="container mx-auto flex min-h-screen flex-col gap-y-3 p-4 md:p-6">
+        <div className="flex h-full grow flex-col items-center justify-center gap-3 md:flex-row md:gap-6">
+          <div className="w-1/3 max-w-[200px]  md:w-1/3">
+            <div className="aspect-square rounded-full border-8 border-white leading-[0] shadow-lg">
+              <Image
+                src="/images/jeffery.jpeg"
+                className="h-auto w-full rounded-full"
+                alt="jeffery"
+                width={0}
+                height={0}
+                sizes="100vw"
+                loader={customImageLoader}
+                unoptimized
+              ></Image>
+            </div>
+          </div>
+          <div className="flex flex-col items-start gap-y-3 p-3 md:w-2/3">
+            <h1 className="w-full text-center text-2xl md:text-start lg:text-5xl">
+              <span className="font-semibold">Jeffery</span> Chia-Chun Ho
+            </h1>
+            <p>
+              I am researching Fake News Detection using deep learning and I also have experience with Frontend and
+              Backend Web development (React, Next.js / Django).
+            </p>
+            <div className="flex w-full flex-row flex-wrap justify-center gap-2 md:justify-start">
+              <LinkButton
+                primaryColor="teal-600"
+                secondaryColor="white"
+                icon="person-circle"
+                href="/files/resume_231212_public.pdf"
+              >
+                Resume
+              </LinkButton>
+              <LinkButton
+                primaryColor="teal-600"
+                secondaryColor="white"
+                icon="mailbox"
+                href="mailto:chiachun2491@gmail.com"
+              >
+                Contact me
+              </LinkButton>
+            </div>
           </div>
         </div>
-        <div className="p-3 md:w-2/3 flex flex-col items-start gap-y-3">
-          <h1 className="text-2xl lg:text-5xl text-center lg:text-start w-full">
-            <span className="font-semibold">Jeffery</span> Chia-Chun Ho
-          </h1>
-          <p>
-            I am researching Fake News Detection using deep learning and I also have experience with Frontend and
-            Backend Web development (React, Next.js / Django).
-          </p>
-          <div className="flex flex-row gap-2 flex-wrap justify-center lg:justify-start">
-            <LinkButton primaryColor="black" secondaryColor="white" icon="github" href="https://github.com/chiachunho">
+        <div className="flex flex-col gap-y-3">
+          <div className="text-lg font-semibold md:text-xl">Quick Links</div>
+          <div className="grid grid-cols-1 gap-3 md:grid-cols-4">
+            <QuickLinkButton
+              primaryColor="black"
+              secondaryColor="white"
+              icon="github"
+              href="https://github.com/chiachunho"
+            >
               @chiachunho
-            </LinkButton>
-            <LinkButton
-              primaryColor="teal-600"
+            </QuickLinkButton>
+            <QuickLinkButton
+              primaryColor="cyan-600"
               secondaryColor="white"
-              icon="mailbox"
-              href="mailto:chiachun2491@gmail.com"
+              icon="journal"
+              href="https://blog.jefferyho.cc"
             >
-              Contact me
-            </LinkButton>
-            <LinkButton
-              primaryColor="teal-600"
-              secondaryColor="white"
-              icon="person-circle"
-              href="/files/resume_231212_public.pdf"
-            >
-              Resume
-            </LinkButton>
-
-            <LinkButton primaryColor="cyan-600" secondaryColor="white" icon="journal" href="https://blog.jefferyho.cc">
               Blog
-            </LinkButton>
-            <LinkButton
+            </QuickLinkButton>
+            <QuickLinkButton
               primaryColor="cyan-600"
               secondaryColor="white"
               icon="file-slides"
               href="https://slides.jefferyho.cc"
             >
               Slides
-            </LinkButton>
+            </QuickLinkButton>
           </div>
         </div>
       </div>
@@ -92,14 +108,30 @@ export default function Home() {
 
 function LinkButton({ primaryColor = 'teal-600', secondaryColor = 'white', icon = 'github', children, href = '#' }) {
   return (
-    <Link href={href}>
-      <a target="_blank" rel="noreferrer noopenner">
-        <div
-          className={`py-1 px-3 cursor-pointer rounded-2xl text-sm font-medium transition-all border-2 border-${primaryColor} bg-${secondaryColor} hover:bg-${primaryColor} text-${primaryColor} hover:text-${secondaryColor} `}
-        >
-          <i className={`bi bi-${icon} mr-1`}></i> {children}
-        </div>
-      </a>
+    <Link href={href} target="_blank" rel="noreferrer noopenner">
+      <div
+        className={`cursor-pointer rounded-2xl border-2 px-3 py-1 text-sm font-medium transition-all border-${primaryColor} bg-${secondaryColor} hover:bg-${primaryColor} text-${primaryColor} hover:text-${secondaryColor} `}
+      >
+        <i className={`bi bi-${icon} mr-1`}></i> {children}
+      </div>
+    </Link>
+  );
+}
+
+function QuickLinkButton({
+  primaryColor = 'teal-600',
+  secondaryColor = 'white',
+  icon = 'github',
+  children,
+  href = '#',
+}) {
+  return (
+    <Link href={href} target="_blank" rel="noreferrer noopenner">
+      <div
+        className={`text-md cursor-pointer rounded-md border-2 px-3 py-1 font-medium transition-all border-${primaryColor} bg-${secondaryColor} hover:bg-${primaryColor} text-${primaryColor} hover:text-${secondaryColor} `}
+      >
+        <i className={`bi bi-${icon} mr-1`}></i> {children}
+      </div>
     </Link>
   );
 }
